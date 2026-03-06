e92746509cb98673cea365399ffb4bed2181fbf7
Miguel Martin <miguelbemartin@gmail.com>
2026-03-06 11:31:58 +0100

    remove md file and add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import {Metadata} from 'next';
+import Script from 'next/script'
 import { ClerkProvider } from '@clerk/nextjs'
 import 'bootstrap/dist/css/bootstrap.css'
 import './style/scss/style.scss'
@@ -23,6 +24,15 @@ export default function RootLayout({
            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
          </head>
         <body>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-XRCNG25M9V" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XRCNG25M9V');
+            `}
+          </Script>
           {children}
         </body>
       </html>
